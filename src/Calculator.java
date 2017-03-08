f7d1bac108818241b77b02b19657b3bc1ae121d3
laivas <emtrys@gmail.com>
2017-03-08 20:12:51 +0200

    added setter and getter

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,14 @@
 public class Calculator {
 
 	private SimpleCalc simpleCalc;
+
+	public SimpleCalc getSimpleCalc() {
+		return simpleCalc;
+	}
+
+	public void setSimpleCalc(SimpleCalc simpleCalc) {
+		this.simpleCalc = simpleCalc;
+	}
+	
 	
 }
