5fb3399278c647c71c7e0ed507c23f9cfc123002
LaurynasM <laurynas.medzevepris@gmail.com>
2017-03-08 14:24:48 +0200

    added SimpleCalc field for simple calculations

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,8 @@
 
 public class Calculator {
 
+	private SimpleCalc simpleCalc;
+	
+	
+
 }
---
+++ src/SimpleCalc.java
@@ -0,0 +1,28 @@
+
+public class SimpleCalc implements Operations{
+
+	
+	
+	@Override
+	public void add(double x, double y) {
+		System.out.println("Result :"+(x+y));
+	}
+
+	@Override
+	public void subtract(double x, double y) {
+		System.out.println("Result :"+(x-y));
+	}
+
+	@Override
+	public void divide(double x, double y) {
+		System.out.println("Result :"+(x/y));
+	}
+
+	@Override
+	public void multiply(double x, double y) {
+		System.out.println("Result :"+(x*y));
+	}
+
+	
+	
+}
