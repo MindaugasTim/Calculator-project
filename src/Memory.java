59a1019346582e62af9931240dc16855e639e163
laivas <emtrys@gmail.com>
2017-03-09 01:52:20 +0200

    no changes

--- src/Memory.java
+++ src/Memory.java
@@ -2,29 +2,26 @@ import java.util.ArrayList;
 
 public class Memory {
 
-	private ArrayList<String> memory = new ArrayList<String>(3);
+	private static ArrayList<String> memory = new ArrayList<String>(3);
 
-	public void addToMemory(String toMemory) {
+	public static void addToMemory(String toMemory) {
 
-			this.memory.add(toMemory);
+		memory.add(toMemory);
 
-		if (this.memory.size() == 3) {
+		if (memory.size() == 3) {
 
-			this.memory.remove(2);
+			memory.remove(2);
 		}
-	
 
 	}
 
-	public void showMemory() {
-		
-		
-		if (memory.size()==0) {
-		 System.out.println("Memory is empty");
-		} 	
-		this.memory.forEach(mem->System.out.println(mem));
-		
-		
+	public static void showMemory() {
+
+		if (memory.size() == 0) {
+			System.out.println("Memory is empty");
+		}
+		memory.forEach(mem -> System.out.println(mem));
+
 	}
 
 }
--- src/TestCalc.java
+++ src/TestCalc.java
@@ -3,12 +3,10 @@ public class TestCalc {
 
 	public static void main(String[] args) {
 		
-		Calculator c1 = new Calculator();
-		c1.setMemory(new Memory());
-		c1.setSimpleCalc(new SimpleCalc());
-		c1.controller();
-		c1.calcMemory();
-		
+		Calculator c1 = new Calculator();		
+		c1.mainMenu();
+
+
 	}
 	
 }
