708c8a602fd74244aae79d577707bba9a2ebec0b
laivas <emtrys@gmail.com>
2017-03-08 22:00:49 +0200

    src/TestCalc.java
    added class for testing

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,49 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	private SimpleCalc simpleCalc;
+	private QuadraticEquation quadraticEquation;
+	private Memory memory;
+	private String result;
+
+	public void controller() {
+
+		Scanner sc = new Scanner(System.in);
+		String i = "";
+		while (i != "stop") {
+			i = sc.next();
+			switch (i) {
+			case ("+"):
+				result = simpleCalc.add(sc.nextDouble(), sc.nextDouble());
+				memory.addToMemory(result);
+				System.out.println(result);
+				break;
+			case ("-"):
+				result = simpleCalc.subtract(sc.nextDouble(), sc.nextDouble());
+				memory.addToMemory(result);
+				System.out.println(result);
+				break;
+			case ("/"):
+				result = simpleCalc.divide(sc.nextDouble(), sc.nextDouble());
+				memory.addToMemory(result);
+				System.out.println(result);
+				break;
+			case ("*"):
+				result = simpleCalc.multiply(sc.nextDouble(), sc.nextDouble());
+				memory.addToMemory(result);
+				System.out.println(result);
+				break;
+			case ("s"):
+				i = "stop";
+				break;
+			default:
+				System.out.println("Choose : + - / or *");
+			}
+
+		}
+
+	}
 
 	public SimpleCalc getSimpleCalc() {
 		return simpleCalc;
@@ -10,6 +52,25 @@ public class Calculator {
 	public void setSimpleCalc(SimpleCalc simpleCalc) {
 		this.simpleCalc = simpleCalc;
 	}
-	
-	
+
+	public QuadraticEquation getQuadraticEquation() {
+		return quadraticEquation;
+	}
+
+	public void setQuadraticEquation(QuadraticEquation quadraticEquation) {
+		this.quadraticEquation = quadraticEquation;
+	}
+
+	public Memory getMemory() {
+		return memory;
+	}
+
+	public void setMemory(Memory memory) {
+		this.memory = memory;
+	}
+
+	public void calcMemory() {
+		memory.showMemory();
+	}
+
 }
